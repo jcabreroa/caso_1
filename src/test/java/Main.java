f86855a49b075984af5b57eb17f25cc5cf5e708b
jca <jcabrero@indra.com>
2022-10-21 13:39:18 +0200

    Subida pom y clase main

---
+++ src/test/java/Main.java
@@ -0,0 +1,19 @@
+/*
+ * @copyright 2022 Indra Sistemas S.A. All rights reserved. INDRA
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author jcabrero
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+
+}
